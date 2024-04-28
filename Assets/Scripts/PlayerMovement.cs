7b667a2007460eafff6bc8911291d973d4d055cd
Alex <50712350+Destro25@users.noreply.github.com>
2024-04-29 02:51:06 +0300

    AddMovementAndCamera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,53 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private Rigidbody2D rigidbodyPlayer;
+    private Animator animPlayer;
+    private SpriteRenderer spritePlayer;
 
+    float xAxis = 0f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpPower = 14f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigidbodyPlayer = GetComponent<Rigidbody2D>();
+        animPlayer = GetComponent<Animator>();
+        spritePlayer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if(Input.GetKeyDown("space"))
+        xAxis = Input.GetAxisRaw("Horizontal");
+        rigidbodyPlayer.velocity = new Vector2(xAxis * moveSpeed, rigidbodyPlayer.velocity.y);
+
+        //Jumping
+        if(Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0,7,0);
+            rigidbodyPlayer.velocity = new Vector2(rigidbodyPlayer.velocity.x, jumpPower);
+        }
+
+        UpdateAnimations();
+    }
+
+    private void UpdateAnimations()
+    {
+        if (xAxis > 0f)
+        {
+            animPlayer.SetBool("IsRunning", true);
+            spritePlayer.flipX = false;
+        }
+        else if (xAxis < 0f)
+        {
+            animPlayer.SetBool("IsRunning", true);
+            spritePlayer.flipX = true;
+        }
+        else
+        {
+            animPlayer.SetBool("IsRunning", false);
         }
     }
+
 }

f483554dc3ef8166a90f19641ff5c056fac0d2a1
Alex <50712350+Destro25@users.noreply.github.com>
2024-06-10 22:08:51 +0300

    add saws

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 1.8f;
+    private void Update()
+    {
+        transform.Rotate(0, 0, 360 * rotationSpeed * Time.deltaTime);
+    }
+}

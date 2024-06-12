cd0435ed262ac459230c2bd7c5ff74dc86d0472b
Alex <50712350+Destro25@users.noreply.github.com>
2024-06-12 04:15:16 +0300

    add dashing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UIElements.Experimental;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -13,11 +15,11 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private LayerMask jumpableTerrain;
 
-    private enum DisplayAnimation {anim_idle, anim_running, anim_jumping, anim_falling, anim_doubleJump, anim_wallHang}
+    private enum DisplayAnimation { anim_idle, anim_running, anim_jumping, anim_falling, anim_doubleJump, anim_wallHang }
 
     float xAxis = 0f;
-    [SerializeField] private float moveSpeed = 7f;
-    [SerializeField] private float jumpPower = 14f;
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float jumpPower = 16f;
 
     private bool hasDoubleJumpe;
     private bool hasWallJump;
@@ -25,6 +27,11 @@ public class PlayerMovement : MonoBehaviour
     private bool isStickingToWall = false;
     [SerializeField] private AudioSource jumpSoundEffect;
     [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private float dashSpeed = 1f;
+    [SerializeField] private float dashTime = 0.2f;
+    [SerializeField] private float dashCooldown = 1f;
+    private bool hasWaited = true;
+    private bool isDashing = false;
     private void Start()
     {
         rigidbodyPlayer = GetComponent<Rigidbody2D>();
@@ -37,62 +44,87 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         xAxis = Input.GetAxisRaw("Horizontal");
-        rigidbodyPlayer.velocity = new Vector2(xAxis * moveSpeed, rigidbodyPlayer.velocity.y);
 
-        StickingToAWall();
-
-        if (Input.GetButtonDown("Jump"))
+        if(!isDashing) 
         {
-            if (TouchingJumpableTerrain() || hasDoubleJumpe || hasWallJump)
+            rigidbodyPlayer.velocity = new Vector2(xAxis * moveSpeed, rigidbodyPlayer.velocity.y);
+            StickingToAWall();
+            if (Input.GetButtonDown("Jump"))
             {
-                jumpSoundEffect.Play();
-                rigidbodyPlayer.velocity = new Vector2(rigidbodyPlayer.velocity.x, jumpPower);
+                if (TouchingJumpableTerrain() || hasDoubleJumpe || hasWallJump)
+                {
+                    jumpSoundEffect.Play();
+                    rigidbodyPlayer.velocity = new Vector2(rigidbodyPlayer.velocity.x, jumpPower);
 
-                if (!TouchingJumpableTerrain())
-                {
-                    hasDoubleJumpe = false;
-                }
-                else
-                {
-                    hasDoubleJumpe = true;
-                }
+                    if (!TouchingJumpableTerrain())
+                    {
+                        hasDoubleJumpe = false;
+                    }
+                    else
+                    {
+                        hasDoubleJumpe = true;
+                    }
 
-                if (!isStickingToWall) 
-                {
-                    hasWallJump = false;
-                }
-                else
-                {
-                    hasWallJump = true;
-                }    
+                    if (!isStickingToWall)
+                    {
+                        hasWallJump = false;
+                    }
+                    else
+                    {
+                        hasWallJump = true;
+                    }
 
+                }
+            }
+
+            if (TouchingJumpableTerrain())
+            {
+                hasDoubleJumpe = true;
+            }
+
+            if (isStickingToWall)
+            {
+                hasWallJump = true;
             }
         }
+        
 
-        if (TouchingJumpableTerrain())
+        if (Input.GetButtonDown("Dash") && hasWaited)
         {
-            hasDoubleJumpe = true;
-        }
-
-        if (isStickingToWall)
-        {
-            hasWallJump = true;
+            StartCoroutine(Dash());
         }
 
         UpdateAnimations();
     }
 
+    private IEnumerator Dash()
+    {
 
+        float originalGravity = rigidbodyPlayer.gravityScale;
+        rigidbodyPlayer.gravityScale = 0f;
+        isDashing = true;
+        hasWaited = false;
+
+        float dashDirection = xAxis != 0f ? xAxis : (spritePlayer.flipX ? -1f : 1f);
+        rigidbodyPlayer.velocity = new Vector2(dashSpeed * dashDirection, 0f);
+        //trail.emitting = true;
+        yield return new WaitForSeconds(dashTime);
+        //trail.emitting = false;
+        rigidbodyPlayer.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        hasWaited = true;
+    }
     private void UpdateAnimations()
     {
         DisplayAnimation status;
 
-        if(isStickingToWall && xAxis > 0f)
+        if (isStickingToWall && xAxis > 0f)
         {
             status = DisplayAnimation.anim_wallHang;
             spritePlayer.flipX = false;
         }
-        else if(isStickingToWall && xAxis < 0f)
+        else if (isStickingToWall && xAxis < 0f)
         {
             status = DisplayAnimation.anim_wallHang;
             spritePlayer.flipX = true;
@@ -115,7 +147,7 @@ public class PlayerMovement : MonoBehaviour
         if (rigidbodyPlayer.velocity.y > 0.01f)
         {
             status = DisplayAnimation.anim_jumping;
-            if(hasDoubleJumpe == false)
+            if (hasDoubleJumpe == false)
             {
                 status = DisplayAnimation.anim_doubleJump;
             }
@@ -150,5 +182,4 @@ public class PlayerMovement : MonoBehaviour
             isStickingToWall = false;
         }
     }
-
 }
